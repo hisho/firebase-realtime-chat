aa8d5c622a5418376ea906a1f4ea9cb0229411bb
hisho <hisho.web@gmail.com>
2022-10-11 20:34:54 +0900

    ✨ getLayoutを実装

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,14 +1,29 @@
-import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { initializeFirebaseApp } from '@src/lib/firebase/initializeFirebase'
 import { theme } from '@src/lib/chakra/theme'
+import { BaseLayout } from '@src/layout/BaseLayout/BaseLayout'
+import type { ReactElement, ReactNode } from 'react'
+import type { AppProps } from 'next/app'
+import type { NextPage } from 'next/types'
 
 initializeFirebaseApp()
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement, pageProps: P) => ReactNode
+}
+
+type AppPropsWithLayout<P = {}> = AppProps<P> & {
+  Component: NextPageWithLayout<P>
+}
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <BaseLayout>
+        {getLayout(<Component {...pageProps} />, pageProps)}
+      </BaseLayout>
     </ChakraProvider>
   )
 }
