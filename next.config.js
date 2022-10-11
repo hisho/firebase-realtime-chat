b257f54c5381f186ca460340afb1bbfb6df62e4c
hisho <hisho.web@gmail.com>
2022-10-11 15:45:11 +0900

    🔧 update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * @see https://nextjs.org/docs/advanced-features/i18n-routing
+   */
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja'],
+  },
+
+  /**
+   * @see https://nextjs.org/docs/api-reference/next.config.js/custom-page-extensions
+   */
+  pageExtensions: ['page.tsx', 'api.ts'],
+  /**
+   * @see https://nextjs.org/docs/api-reference/next.config.js/disabling-x-powered-by
+   */
+  poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true,
 }
