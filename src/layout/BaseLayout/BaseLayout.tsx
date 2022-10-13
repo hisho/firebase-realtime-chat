0364ec3b542aa8e0135f3aac0b95c15fb8697970
hisho <hisho.web@gmail.com>
2022-10-13 12:45:06 +0900

    ✨ BaseLayoutにHeaderを追加

--- src/layout/BaseLayout/BaseLayout.tsx
+++ src/layout/BaseLayout/BaseLayout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 import { Box, Flex } from '@chakra-ui/react'
 import { Footer } from '@src/layout/Footer/Footer'
+import { Header } from '@src/layout/Header/Header'
 
 type Props = {
   children: ReactNode
@@ -9,6 +10,7 @@ type Props = {
 export const BaseLayout = ({ children }: Props) => {
   return (
     <Flex flexDirection={'column'} h={'inherit'}>
+      <Header />
       <Box flex={1}>{children}</Box>
       <Box mt={'auto'}>
         <Footer />
