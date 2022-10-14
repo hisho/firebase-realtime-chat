8e7ced2d378caa3849181f2d39378705938c44fb
hisho <hisho.web@gmail.com>
2022-10-14 15:40:56 +0900

    🐛 CreateChatMessageInputをzodに食わせた時のエラー修正

--- src/feature/chat/model/Chat.ts
+++ src/feature/chat/model/Chat.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
 import { serverTimestamp } from '@firebase/database'
 import type { AuthState } from '@src/feature/auth/provider/AuthProvider/AuthProvider'
+import type { ToZod } from '@src/lib/zod/types'
+import { valueAsString } from '@hisho/utils'
 
 export type CreateChatMessageInput = {
   message: string
@@ -11,14 +13,22 @@ export type CreateChatMessageInput = {
   createdAt: unknown
 }
 
-export const createChatMessageSchema = z.object({
-  message: z.string().min(1).max(100),
-  createdAt: z.unknown(),
-  user: z.object({
-    name: z.preprocess((v) => v || '未設定', z.string()),
-    avatarUrl: z.string(),
-  }),
-})
+export const createChatMessageSchema = z
+  .object<ToZod<CreateChatMessageInput>>({
+    message: z.string().min(1).max(100),
+    createdAt: z.unknown(),
+    user: z.object({
+      name: z.string(),
+      avatarUrl: z.string(),
+    }),
+  })
+  .transform(({ user, ...others }) => ({
+    ...others,
+    user: {
+      ...user,
+      name: valueAsString(user.name, { defaultValue: '未設定' }),
+    },
+  }))
 
 export const createChatMessageDefaultValues = (user: AuthState) => {
   return {
