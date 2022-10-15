022cbc8ee6d581e73d107a458571ed1bdba84811
hisho <hisho.web@gmail.com>
2022-10-16 03:49:58 +0900

    ✨ SendIconを作成

---
+++ src/component/Icon/SendIcon/SendIcon.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, BaseIconProps } from '@src/component/Icon/BaseIcon'
+import { BiSend } from 'react-icons/bi'
+
+export const SendIcon = (props: BaseIconProps) => {
+  return <BaseIcon icon={BiSend} {...props} />
+}
