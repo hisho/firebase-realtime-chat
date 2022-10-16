c6d384571438b287b081527930643824a747b1dd
hisho <hisho.web@gmail.com>
2022-10-16 10:05:38 +0900

    ✨ 認証アイコンを作成

---
+++ src/component/Icon/VerifiedIcon/VerifiedIcon.tsx
@@ -0,0 +1,6 @@
+import { BaseIcon, BaseIconProps } from '@src/component/Icon/BaseIcon'
+import { MdVerified } from 'react-icons/md'
+
+export const VerifiedIcon = (props: BaseIconProps) => {
+  return <BaseIcon icon={MdVerified} {...props} />
+}
