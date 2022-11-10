b93eac2ea0f8701451913a6e6ef21c8ffbf05079
hisho <hisho.web@gmail.com>
2022-11-10 18:32:36 +0900

    ✨ トップページのルームにloading fallbackを追加

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -5,25 +5,34 @@ import { Box, chakra, Grid, Heading } from '@chakra-ui/react'
 import { Navigate } from '@src/component/Navigate/Navigate'
 import { RoomCard } from '@src/feature/rooms/component/RoomCard/RoomCard'
 import { Spacer } from '@src/component/Spacer/Spacer'
+import { Range } from '@src/component/Rnage/Rnage'
 
 const Page: NextPageWithLayout = () => {
-  const { rooms } = useRooms({ limit: 4 })
+  const { rooms, isLoading } = useRooms({ limit: 4 })
 
   return (
     <Box py={10}>
       <Heading fontSize={'24px'}>最近作成されたルーム</Heading>
       <Spacer h={4} />
       <Grid gridTemplateColumns={'repeat(2,1fr)'} gap={2}>
-        {rooms.map((room, index) => (
-          <Navigate
-            key={`RoomCard_${room.uid}_${index}`}
-            href={(path) => path.rooms._room_uid(room.uid).chat.$url()}
-          >
-            <chakra.a _hover={{ opacity: 0.8 }}>
-              <RoomCard room={room} />
-            </chakra.a>
-          </Navigate>
-        ))}
+        {isLoading ? (
+          <Range length={4}>
+            {({ index }) => (
+              <RoomCard.Skeleton key={`RoomCard.Skeleton_${index}`} />
+            )}
+          </Range>
+        ) : (
+          rooms.map((room, index) => (
+            <Navigate
+              key={`RoomCard_${room.uid}_${index}`}
+              href={(path) => path.rooms._room_uid(room.uid).chat.$url()}
+            >
+              <chakra.a _hover={{ opacity: 0.8 }}>
+                <RoomCard room={room} />
+              </chakra.a>
+            </Navigate>
+          ))
+        )}
       </Grid>
     </Box>
   )
