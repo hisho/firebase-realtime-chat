91b902219ddf60d83ba9a42f830a54983d8ef146
hisho <hisho.web@gmail.com>
2022-11-10 19:16:24 +0900

    ✨ サインインした時のqueryにredirectがあればそのページに遷移するように

--- src/pages/signin/index.page.tsx
+++ src/pages/signin/index.page.tsx
@@ -5,11 +5,28 @@ import { BaseLayout } from '@src/layout/BaseLayout/BaseLayout'
 import { useSignInForm } from '@src/feature/auth/component/SignInForm/useSignInForm'
 import { useNavigate } from '@src/hooks/useNavigate/useNavigate'
 import { SignInForm } from '@src/feature/auth/component/SignInForm/SignInForm'
+import { useRouter } from '@src/hooks/useRouter/useRouter'
+import { z } from 'zod'
+
+export type OptionalQuery = {
+  redirect: string
+}
 
 const Page: NextPageWithLayout = () => {
   const { push } = useNavigate()
+  const { query } = useRouter()
+  const parsedQueryParams = z
+    .object({
+      redirect: z.string(),
+    })
+    .safeParse(query)
   const { form, handleSignIn, isLoading } = useSignInForm({
     onCompleted: () => {
+      if (parsedQueryParams.success) {
+        const { redirect } = parsedQueryParams.data
+        const [pathname, search] = redirect.split('?')
+        push({ pathname, search })
+      }
       push((path) => path.$url())
     },
   })
